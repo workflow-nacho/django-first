945be452dc028d4824e324d2d13f8b5cefdd55c4
Nacho <joseignaciorm13@gmail.com>
2020-03-26 13:22:59 +0100

    Added in blog app:  template: (base, home, about).html and static: main.css -> Added  BOOTSTRAP 4 for CSS styles

--- django_first/blog/views.py
+++ django_first/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Creating Fake posts into a list with some information in a dictionary
+posts = [
+    {
+        'author': 'CoreyMS',
+        'student': 'Nacho',
+        'title': 'Blog post one',
+        'content': 'First post content',
+        'date_posted': 'March 26, 2020'
+    },
+    {
+        'author': 'Nacho RM',
+        'title': 'Blog post two',
+        'content': 'Second post content',
+        'date_posted': 'March 27, 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog-home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', { 'title': 'About' })
